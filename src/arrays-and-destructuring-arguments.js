956249209dae1fecdb3c6eb8e54c0d9b0a1be6b4
suxxus <suxxus@outlook.com>
2016-08-16 15:10:30 +0200

    destructuring arrays

--- src/arrays-and-destructuring-arguments.js
+++ src/arrays-and-destructuring-arguments.js
@@ -1,1 +1,15 @@
 // https://leanpub.com/javascriptallongesix/read#arraysanddestructuring
+
+// in JavaScript, we can actually reverse the statement and place the template on
+// the left and a value on the right:
+
+const unwrap = value => {
+  const [
+    val1,
+    val2
+  ] = value;
+
+  return `value1: ${val1}, value2: ${val2}`;
+};
+
+console.log('---> ' + unwrap(['Peter','John','Lois']) );
