cfeb5238b5ccc3e2c5975f3f5734244714c7313f
suxxus <suxxus@outlook.com>
2016-08-24 13:27:21 +0200

    linear recursion es5 es6

--- src/linear-recursion.js
+++ src/linear-recursion.js
@@ -6,16 +6,23 @@
 //    If a smaller problem is not solvable, divide and conquer that problem
 //    When all small problems have been solved, compose the solutions into one big solution
 
-var arr = [];
-var arr1 = [...[1, 2, 3]];
-var arr2 = [1, ...[]];
-var arr3 = [1, ...[2, 3]];
+const arr = [];
+const arr1 = [...[1, 2, 3]];
+const arr2 = [1, ...[]];
+const arr3 = [1, ...[2, 3]];
+const arr4 = [...['a', 'b'], ...['x', 'y']];
+const [value1, ...value2] = [
+    ['Pep'], 'Rita', 'Alice', 'Joana'
+];
 
 console.log(
     arr,
     arr1,
     arr2,
-    arr3
+    arr3,
+    arr4,
+    value1,
+    value2
 );
 
 var ln = function(value) {
@@ -28,19 +35,89 @@ console.log(ln([1, 2, 3]));
 const len = ([first, ...rest]) => !first ? 0 : 1 + len(rest);
 console.log(len([4, 5, 6, 7]));
 
+
+// --------------------------------------------
 var flattenEmpty = function(value) {
-    return value.length === 0 ? [] : '';
+    return value.length === 0 ? [] : value;
 };
 console.log(
     'flattening an empty array will produce an empty array.',
     flattenEmpty([])
 );
 
-var elemIsNotArrayType = function fl(value) {
-    var first = value.shift();
-    return !Array.isArray(first) ? value.slice(0) : '';
+var elemIsNotArrayType = function(value) {
+    var firstIsNotArray = !Array.isArray(value[0]);
+    if (firstIsNotArray) {
+        var first = value.shift();
+        return [first];
+    }
+
+    return value;
 };
 console.log(
     'if an element isn’t an array, we don’t flatten it',
     elemIsNotArrayType(['a', 'b'])
+);
+
+var concatFirstElement = function(value) {
+    var first = value.shift();
+    var rest = value;
+    var firstIsArray = Array.isArray(first);
+
+    if (firstIsArray) {
+        return [].concat(first);
+    }
+
+    return rest;
+};
+console.log(
+    'if first element is an array we flatten it and put it with the rest',
+    concatFirstElement([
+        ['a'], 'b', ['c']
+    ])
+);
+
+
+var flattenArray = function(value) {
+
+    if (value.length === 0) {
+        return [];
+    }
+
+    if (!Array.isArray(value[0])) {
+        var first = value.shift();
+        return [first]
+            .concat(flattenArray(value));
+    }
+
+    return []
+        .concat(flattenArray(value.shift()))
+        .concat(flattenArray(value));
+};
+
+console.log(
+    'flatten Array',
+    flattenArray([
+        ['a'], 'b', ['c']
+    ])
+);
+
+const flattenArrayEs6 = ([first, ...rest]) => {
+
+    if (!first) {
+        return [];
+    }
+
+    if (!Array.isArray(first)) {
+        return [first, ...flattenArrayEs6(rest)];
+    }
+
+    return [...flattenArrayEs6(first), ...flattenArrayEs6(rest)];
+};
+
+console.log(
+    'flattenArrayEs6: ',
+    flattenArrayEs6([
+        ['a'], 'b', ['c']
+    ])
 );
