cbd42401f5cf1fb7863c7e8140a86e185fc81439
suxxus <suxxus@outlook.com>
2016-08-30 16:28:48 +0200

    remove comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
  import 'arrays-and-destructuring-arguments';
-// import 'linear-recursion';
-// import 'linear-recursion-mapping';
-// import 'linear-recursion-folding';
-// import 'tail-calls-and-default-arguments';
+ import 'linear-recursion';
+ import 'linear-recursion-mapping';
+ import 'linear-recursion-folding';
+ import 'tail-calls-and-default-arguments';
