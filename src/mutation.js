fced86603df9b7edc9321f42a495a70f8856f646
suxxus <suxxus@outlook.com>
2016-11-03 10:03:50 +0100

    mutation structured data

--- src/mutation.js
+++ src/mutation.js
@@ -11,12 +11,45 @@
 // most important point about this is that the identities
 // were the same, because they were the same value
 const allHallowsEve = [2012, 10, 31];
-(function (halloween) {
-  console.log('should equal--> ',  String(halloween) === String(allHallowsEve));
+(function(halloween) {
+    console.log('should equal--> ', String(halloween) === String(allHallowsEve));
 })(allHallowsEve);
 
 
-(function (halloween) {
-  halloween = [2016, 10, 31];
-  console.log('should not equal--> ',  String(halloween) !== String(allHallowsEve));
+(function(halloween) {
+    halloween = [2016, 10, 31];
+    console.log('should not equal--> ', String(halloween) !== String(allHallowsEve));
 })(allHallowsEve);
+
+
+console.log('======== END - mutation - =========\n');
+
+// One pattern many people follow is to be liberal with mutation 
+// when constructing data, but conservative with mutation when consuming data
+
+const oneToFive = {
+    value: 'one',
+    next: {
+        value: 'two',
+        next: {
+            value: 'three',
+            next: {
+                value: 'four',
+                next: {
+                    value: 'five',
+                    next: {}
+                }
+            }
+        }
+    }
+};
+console.log('oneToFive ---> ', oneToFive);
+
+const threeToFive = oneToFive.next.next;
+console.log('threeToFive ---> ', threeToFive);
+
+threeToFive.next.value = 4;
+console.log('mutated threeToFive ---> ', JSON.stringify(threeToFive));
+console.log('mutated oneToFive ---> ', JSON.stringify(oneToFive));
+
+console.log('======== END - mutation and data structures - =========\n');
